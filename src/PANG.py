7f3af2052e979b407246e856d3e0f6d9feeedf77
Lucas Potin <lpotin888@gmail.com>
2023-03-30 15:10:24 +0200

    CodeFonctionnel

--- src/PANG.py
+++ src/PANG.py
@@ -134,7 +134,7 @@ def load_graphs(fileName,TAILLE):
         for j in range(int(edges[i])):
             graphes[i].add_edge(labelEdges[i][j][0],labelEdges[i][j][1],color=labelEdges[i][j][2])
         graphes[i].add_nodes_from([(node, {'color': attr}) for (node, attr) in dicoNodes.items()])
-    return graphes,numbers,noms,nbV,nbE
+    return graphes,numbers,noms
 
 def load_patterns(fileName,TAILLE):
     """ This function loads the post-processed patterns, i.e with occurences.
@@ -236,27 +236,22 @@ def computeScoreMono(keep,labels,id_graphs,TAILLEPATTERN):
     NbRed = sum(labels)
     NbNonRed = len(labels)-NbRed
     diffDelta=np.zeros(TAILLEPATTERN)
-    tailleRed =[]
-    tailleNonRed=[]
     ##Delta Criterion
     for i in range(len(diffDelta)):
         if len(id_graphs[i])>0:
                 for j in range(len(id_graphs[i])):
-                        if id_graphs[i][j] in keep:
                             aa=id_graphs[i][j]
                             if labels[aa]==0:
                                         diffDelta[i]=diffDelta[i]+1
-                                        tailleNonRed[i]=tailleNonRed[i]+1
                             elif labels[aa]==1:
                                         diffDelta[i]=diffDelta[i]-1
-                                        tailleRed[i]=tailleRed[i]+1
-        
         diffDelta[i] = abs(diffDelta[i])
+    print(diffDelta)
     return diffDelta
     
 ###################################
             
-def computeScoreOccurences(keep,occurences,labels,id_graphs,TAILLEPATTERN):
+def computeScoreOccurences(keep,labels,id_graphs,occurences,TAILLEPATTERN):
     """ this function computes the discrimination score of each pattern using the Integer Criterion"""
     """ Input : keep (list of int) : the list of patterns to keep
                 occurences (list of list of int) : the list of number occurences of each pattern in each graph
@@ -271,29 +266,24 @@ def computeScoreOccurences(keep,occurences,labels,id_graphs,TAILLEPATTERN):
     diffCORK=np.zeros(TAILLEPATTERN)
     tailleRed =[]
     tailleNonRed=[]
-    coverageRed=[]
-    coverageNonRed=[]
     for i in range(TAILLEPATTERN):
         tailleRed.append(0)
         tailleNonRed.append(0)
-        coverageRed.append(0)
-        coverageNonRed.append(0)
     ##Delta Criterion
     for i in range(len(diffDelta)):
         if len(id_graphs[i])>0:
                 for j in range(len(id_graphs[i])):
                     if labels[id_graphs[i][j]]==0:
-                        if j in keep:
-                                diffDelta[i]=diffDelta[i]+occurences[i][j]
-                                tailleNonRed[i]=tailleNonRed[i]+occurences[i][j]
+                            diffDelta[i]=diffDelta[i]+occurences[i][j]
+                            tailleNonRed[i]=tailleNonRed[i]+occurences[i][j]
                     elif labels[id_graphs[i][j]]==1:
-                        if j in keep:
-                                diffDelta[i]=diffDelta[i]-occurences[i][j]
-                                tailleRed[i]=tailleRed[i]+occurences[i][j]
+                            diffDelta[i]=diffDelta[i]-occurences[i][j]
+                            tailleRed[i]=tailleRed[i]+occurences[i][j]
         diffDelta[i] = abs(diffDelta[i])
-        return diffDelta
+    print(diffDelta)
+    return diffDelta
     
-def recupLabel(fileLabel):
+def readLabels(fileLabel):
     """ this function reads the file containing the labels of the graphs
         and convert them into 2 classes : 0 and 1
         
@@ -312,7 +302,7 @@ def recupLabel(fileLabel):
         numero=numero+1
     return labels
 
-def KVector(K,diff,id_graphs,numberoccurences,LENGTHGRAPH,labels):
+def KVector(keep,K,diff,id_graphs,numberoccurences,LENGTHGRAPH):
     """ this fuction creates the vectorial representation of the graphs
         Input : K (int) : the number of patterns to keep
         diff (list of int) : the list of discrimination scores of each pattern
@@ -348,104 +338,8 @@ def KVector(K,diff,id_graphs,numberoccurences,LENGTHGRAPH,labels):
                 else:
                     vectorialRep[j].append(0)
     X = vectorialRep
-    Y = newLabels
-    return X,Y
+    return X
 
-def test(X,Y,classifier):
-    test_pred_decision_tree = classifier.predict(X)
-    confusion_matrix = metrics.confusion_matrix(Y,  
-                                               test_pred_decision_tree)#turn this into a dataframe
-    matrix_df = pd.DataFrame(confusion_matrix)#plot the result
-    ax = plt.axes()
-    plt.figure(figsize=(10,7))
-    ax.set_title('Confusion Matrix - Decision Tree')
-    ax.set_xlabel("Predicted label", fontsize =15)
-    ax.set_ylabel("True Label", fontsize=15)
-    plt.savefig("foo.pdf")
-    #plt.show()
-
-def Expe(keep,Ks,fileLabel,out,id_graphs,occurences,coverage,MODE,MotifGardes,TAILLEGRAPHE,TAILLEPATTERN,cv,metric):
-    patterns = []
-    labels,coefficient =recupLabel(fileLabel)
-    file = open(out,"a")
-    if MODE == "support":
-        diffDelta = computeScoreMono(keep,labels,id_graphs,TAILLEPATTERN,1)
-    if MODE =="occurences":
-        diffDelta = computeScoreOccurences(keep,occurences,coverage,labels,id_graphs,TAILLEPATTERN,1)
-    for K in Ks:
-        nom=str(K)+"Prediction.txt"
-        fileBis=open(nom,"w")
-        comc=0
-        if MotifGardes==None:
-            if metric =="DELTA":
-                temp = copy.deepcopy(diffDelta)
-                Xt,Yt,numo,DiscriPat,RED,NONRED = KVector(NbRed,NbNonRed,keep,K,id_graphs,occurences,None,temp,TAILLEGRAPHE,TAILLEPATTERN,labels)
-            if metric =="CORK":
-                temp = copy.deepcopy(diffCORK)
-                Xt,Yt,numo,DiscriPat,RED,NONRED = KVector(NbRed,NbNonRed,keep,K,id_graphs,occurences,None,temp,TAILLEGRAPHE,TAILLEPATTERN,labels)
-        else:
-            temp = copy.deepcopy(diffDelta)
-            Xt,Yt,numo,DiscriPat,RED,NONRED = KVector(NbRed,NbNonRed,keep,K,id_graphs,occurences,MotifGardes,temp,TAILLEGRAPHE,TAILLEPATTERN,labels)
-        for e in range(len(DiscriPat)):
-            stre = "Motif "+ str(DiscriPat[e])+ "score "+str(numo[e])+ "NBRED "+str(RED[e])+" NONRED "+str(NONRED[e])+" \n"
-            fileBis.write(stre)
-    
-        X = []
-        Y = []
-        for j in range(len(Xt)):
-            if not(Yt[j]=="miss"):
-                X.append(Xt[j])
-                Y.append(Yt[j])
-        
-        from sklearn.model_selection import train_test_split
-        from sklearn.model_selection import cross_validate
-        from sklearn import tree
-        import graphviz
-        from sklearn.ensemble import GradientBoostingClassifier
-        from sklearn.svm import SVC
-        nummmm=-1
-        classifiers = [0.1,1,10,100,1000,10000,100000]
-        from sklearn.model_selection import cross_val_score
-        for c in classifiers:
-                class1=[0,0,0,0,0,0,0,0,0,0]
-                class2=[0,0,0,0,0,0,0,0,0,0]
-                class3=[0,0,0,0,0,0,0,0,0,0]
-                class0=[0,0,0,0,0,0,0,0,0,0]
-                acc=[0,0,0,0,0,0,0,0,0,0]
-                f_score=[0,0,0,0,0,0,0,0,0,0]
-                from sklearn import metrics
-                from sklearn.model_selection import StratifiedKFold
-                ccc=0
-                for train_index, test_index in cv[0].split(X,Y):
-                        clf = SVC(C=c)
-                        X_train=[]
-                        X_test=[]
-                        y_train=[]
-                        y_test=[]
-                        for l in train_index:
-                            X_train.append(X[l])
-                            y_train.append(Y[l])
-                        for l in test_index:
-                            X_test.append(X[l])
-                            y_test.append(Y[l])
-                        clf = clf.fit(X_train,y_train)
-                        y_pred = clf.predict(X_test)
-                        ci=0
-                        acc[ccc] = metrics.accuracy_score(y_test, y_pred)
-                        class3[ccc] = metrics.recall_score(y_test, y_pred, average=None)[0]
-                        class1[ccc] = metrics.f1_score(y_test, y_pred, average=None)[1]
-                        class2[ccc] = metrics.precision_score(y_test, y_pred, average=None)[0]
-                        class0[ccc] =  metrics.f1_score(y_test, y_pred, average=None)[0]
-                        f_score[ccc] = metrics.f1_score(y_test, y_pred, average=None)[0]
-                        ccc=ccc+1
-                stre = str(len(X[0]))+ " & "+ str(np.mean(class0))[0:9]+ " & "+ str(np.std(class0))[0:9]+ " & "+ str(np.mean(class1))[0:9]+ " & "+ str(np.std(class1))[0:9]+ " & "+ str(np.mean(acc))[0:9]+" & "+ str(np.mean(f_score))[0:9]+" \\ \n"
-                file.write(stre)
-                y_pred = clf.predict(X)
-                if c==1000:
-                    for teta in range(len(y_pred)):
-                            stre = "NumeroGraphe"+str(keep[teta])+ " Realite "+str(Y[teta])+"Predi "+str(y_pred[teta])+" Vecto "+str(sum(X[teta]))+" \n"
-                            fileBis.write(stre)
-    return numo,patterns
 import sys, getopt
 
 def graphKeep(PatternRed,fileLabel,nbV,nbE,MegaRES):
@@ -511,8 +405,6 @@ def cross_validation(X,Y,cv,classifier):
     #store for each fold the F1 score of each class
     F1_score0 = []
     F1_score1 = []
-    #reset the classifier
-    classifier.reset()
     # for each fold
     for train_index, test_index in cv.split(X,Y):
         #split the dataset into train and test
@@ -526,13 +418,16 @@ def cross_validation(X,Y,cv,classifier):
         for l in test_index:
             X_test.append(X[l])
             y_test.append(Y[l])
-        #train the classifier
-        classifier.train(X_train,y_train)
-        #test the classifier
-        classifier.test(X_test,y_test)
-        #add the F1 score of each class to the array
-        F1_score0.append(classifier.F1_score0)
-        F1_score1.append(classifier.F1_score1)
+        #fit the classifier on the train set
+        classifier.fit(X_train,y_train)
+        #predict the labels of the test set
+        y_predict = classifier.predict(X_test)
+        #find the F1 score of each class on the test set
+        # use the function for finding the f score between two labels sets 
+        # using sklearn.metrics
+        
+        F1_score0.append(metrics.f1_score(y_test, y_predict, average=None)[0])
+        F1_score1.append(metrics.f1_score(y_test, y_predict, average=None)[0])
     #compute the mean and standard deviation of the F1 score of each class
     F1_score0_mean = np.mean(F1_score0)
     F1_score0_std = np.std(F1_score0)
@@ -542,22 +437,22 @@ def cross_validation(X,Y,cv,classifier):
     return F1_score0_mean,F1_score0_std,F1_score1_mean,F1_score1_std
 
 
-def pangProcessing(keep,labels,id_graphs_mono,id_graphs_iso,TAILLEPATTERN):
+def pangProcessing(Ks,keep,labels,id_graphs_mono,id_graphs_iso,occurences_mono,occurences_iso,LENGTHPATTERN,LENGTHGRAPHS):
     cv = StratifiedKFold(n_splits=10,shuffle=True)
-    scoresGenBin = computeScoreMono(keep,labels,id_graphs_mono,TAILLEPATTERN,1)
-    scoresGenOcc = computeScoreMono(keep,labels,id_graphs_iso,TAILLEPATTERN,1)
-    scoresIndBin = computeScoreOccurences(keep,labels,id_graphs_iso,TAILLEPATTERN,1)
-    scoresIndOccn = computeScoreOccurences(keep,labels,id_graphs_iso,TAILLEPATTERN,1)
+    scoresGenBin = computeScoreMono(keep,labels,id_graphs_mono,LENGTHPATTERN)
+    scoresGenOcc = computeScoreOccurences(keep,labels,id_graphs_mono,occurences_mono,LENGTHPATTERN)
+    scoresIndBin = computeScoreMono(keep,labels,id_graphs_iso,LENGTHPATTERN)
+    scoresIndOcc = computeScoreOccurences(keep,labels,id_graphs_iso,occurences_iso,LENGTHPATTERN)
     for K in Ks:
-        X_GenBin,Y_GenBin,numo,DiscriPat,RED,NONRED = KVector(keep,K,id_graphs,occurences,None,temp,TAILLEGRAPHE,TAILLEPATTERN,labels)
-        X_GenOcc,Y_GenOcc,numo,DiscriPat,RED,NONRED = KVector(keep,K,id_graphs,occurences,None,temp,TAILLEGRAPHE,TAILLEPATTERN,labels)
-        X_IndBin,Y_IndBin,numo,DiscriPat,RED,NONRED = KVector(keep,K,id_graphs,occurences,None,temp,TAILLEGRAPHE,TAILLEPATTERN,labels)
-        X_IndOcc,Y_IndOcc,numo,DiscriPat,RED,NONRED = KVector(keep,K,id_graphs,occurences,None,temp,TAILLEGRAPHE,TAILLEPATTERN,labels)
+        X_GenBin = KVector(keep,K,scoresGenBin,id_graphs_mono,None,LENGTHGRAPHS)
+        X_GenOcc = KVector(keep,K,scoresGenOcc,id_graphs_mono,occurences_mono,LENGTHGRAPHS)
+        X_IndBin = KVector(keep,K,scoresIndBin,id_graphs_iso,None,LENGTHGRAPHS)
+        X_IndOcc = KVector(keep,K,scoresIndOcc,id_graphs_iso,occurences_iso,LENGTHGRAPHS)
         results = np.zeros((4,2,2))
-        representations = [[X_GenBin,Y_GenBin],[X_GenOcc,Y_GenOcc],[X_IndBin,Y_IndBin],[X_IndOcc,Y_IndOcc]]:
+        representations = [[X_GenBin,labels],[X_GenOcc,labels],[X_IndBin,labels],[X_IndOcc,labels]]
         for i in range(len(representations)):
-            results[i][0][0],results[i][0][1],results[i][1][0],results[i][1][1] = cross_validation(representations[i][0],representations[i][1],cv,SVC(c=1000))
-        outputResults(K,results,"data/"+str(K)+"results.txt")
+            results[i][0][0],results[i][0][1],results[i][1][0],results[i][1][1] = cross_validation(representations[i][0],representations[i][1],cv,SVC(C=1000))
+        outputResults(K,results,"../data/"+str(K)+"results.txt")
     return 0
 
 def outputResults(K,results, output_file):
@@ -565,9 +460,9 @@ def outputResults(K,results, output_file):
         Input : K : number of patterns
         results : array containing the results of the cross validation
         output_file : file where the results will be written"""
-    with open(output_file, 'w') as f:
-        f.write("Results for K = "+str(K)+ " \n")
-        for i in range(len(results)):
+    f = open(output_file,"w")
+    f.write("Results for K = "+str(K)+ " \n")
+    for i in range(len(results)):
             f.write("Representation "+str(i)+"\n")
             f.write("F1 score for class 0: "+str(results[i][0][0])+" +/- "+str(results[i][0][1])+"\n")
             f.write("F1 score for class 1: "+str(results[i][1][0])+" +/- "+str(results[i][1][1])+"\n")
@@ -584,7 +479,7 @@ def main(argv):
          print ('PANG.py -d <dataset> -k<values of K> -m<mode>')
          sys.exit()
       elif opt in ("-d"):
-            folder="data/"
+            folder="../data/"+str(arg)+"/"
             FILEGRAPHS=folder+str(arg)+"_graph.txt"
             FILESUBGRAPHS=folder+str(arg)+"_pattern.txt"
             FILEMONOSET=folder+str(arg)+"_mono.txt"
@@ -605,21 +500,20 @@ def main(argv):
             keep.append(i)
             
     """loading graphs"""
+    print("Reading graphs")
     Graphes,useless_var,PatternsRed= load_graphs(FILEGRAPHS,TAILLEGRAPHE)
     """loading patterns"""
+    print("Reading patterns")
     Subgraphs,id_graphs,noms = load_graphs(FILESUBGRAPHS,TAILLEPATTERN)
     
     """loading processed patterns"""
+    print("Reading processed patterns")
     xx,id_graphsMono,numberoccurencesMono = load_patterns(FILEMONOSET,TAILLEPATTERN)
     xx,id_graphsIso,numberoccurencesIso = load_patterns(FILEISOSET,TAILLEPATTERN)
-    keep = graphKeep(PatternsRed,FILELABEL)
-    
-    
-    numoA,pattA =Expe(keep,Ks,FILELABEL,"DecoMonoCGZ.txt",id_graphs,None,None,"Support",None,TAILLEGRAPHE,TAILLEPATTERN,cv,"DELTA")
-    numoB,pattB =Expe(keep,Ks,FILELABEL,"DecoOccu.txt",id_graphsMono,numberoccurencesMono,coverageMono,"Occurences",None,TAILLEGRAPHE,TAILLEPATTERN,cv,"DELTA")
-    numoC,pattC =Expe(keep,Ks,FILELABEL,"DecoIso.txt",id_graphsIso,None,coverageIso,"Support",None,TAILLEGRAPHE,TAILLEPATTERN,cv,"DELTA")
-    numoD,pattD =Expe(keep,Ks,FILELABEL,"DecoDouble.txt",id_graphsIso,numberoccurencesIso,coverageIso,"Occurences",None,TAILLEGRAPHE,TAILLEPATTERN,cv,"DELTA")
-
+    #keep = graphKeep(PatternsRed,FILELABEL)
+    labels = readLabels(FILELABEL)
+    print("Processing PANG")
+    pangProcessing(Ks,keep,labels,id_graphsMono,id_graphsIso,numberoccurencesMono,numberoccurencesIso,TAILLEPATTERN,TAILLEGRAPHE)
 if __name__ == "__main__":
    print(sys.argv)
    main(sys.argv[1:])
