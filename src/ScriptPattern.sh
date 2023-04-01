0aefa8811827693a468863d6db61df12567c7241
Lucas Potin <lpotin888@gmail.com>
2023-04-01 14:58:21 +0200

    NEttoyage

--- src/ScriptPattern.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash 
-
-java -Xmx8192m -jar spmf.jar run GSPAN ../data/MUTAG/MUTAG_graph.txt ../data/MUTAG/MUTAG_pattern.txt 0.00001 8 true false true 
-java -Xmx8192m -jar spmf.jar run CGSPANSupport ../data/MUTAG/MUTAG_graph.txt ../data/MUTAG/MUTAG_CG.txt 0.00001 8 true false true 
-python3 ProcessingPatterns.py -d MUTAG
